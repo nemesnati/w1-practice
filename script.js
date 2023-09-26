46c561226d91d2e4f976703135f2b2f87a267eb3
nemesnati <nemes.nati86@gmail.com>
2023-09-26 12:35:02 +0200

    variable

---
+++ script.js
@@ -0,0 +1,10 @@
+let variable = "kis"
+let variable2 = "macska"
+let number = 5
+let number2 = 10
+
+//console.log(100 + 8)
+//console.log("100" + "8")
+console.log("variable")
+console.log(variable + variable2)
+console.log(number+number2)
