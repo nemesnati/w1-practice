ef7f803db51a02d43e0990441593656399c5f18b
nemesnati <nemes.nati86@gmail.com>
2023-09-26 19:53:39 +0200

    homework

---
+++ script2.js
@@ -0,0 +1,12 @@
+function házi(param) {
+    console.log(param)
+  }
+  
+  házi ("pálinka") 
+  
+  function productOfThreeNumbers(number1, number2, number3) {
+    let sum = number1 * number2 * number3
+    return sum 
+  }
+  
+házi(productOfThreeNumbers(4, 8, 5))
