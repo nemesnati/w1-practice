6a2549113a11ee8fded1c0fd0800733aac66c9fb
nemesnati <nemes.nati86@gmail.com>
2023-09-27 10:33:37 +0200

    productOfFour

--- script2.js
+++ script2.js
@@ -4,8 +4,8 @@ function getResult(param) {
   
   getResult ("kiskutya") 
   
-  function productOfThreeNumbers(number1, number2, number3) {
-    let product = number1 * number2 * number3
+  function productOfFourNumbers(number1, number2, number3, number4) {
+    let product = number1 * number2 * number3 * number4
     return product 
   }
-  getResult(productOfThreeNumbers(136, 278, 95))
+  getResult(productOfFourNumbers(136, 278, 95, 639))
