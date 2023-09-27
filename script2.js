562a234265b5966e20b04dc243c2c2eb75bdf815
nemesnati <nemes.nati86@gmail.com>
2023-09-27 09:00:45 +0200

    totally finished

--- script2.js
+++ script2.js
@@ -2,7 +2,7 @@ function getResult(param) {
     console.log(param)
   }
   
-  getResult ("gizsgutya") 
+  getResult ("kiskutya") 
   
   function productOfThreeNumbers(number1, number2, number3) {
     let sum = number1 * number2 * number3
